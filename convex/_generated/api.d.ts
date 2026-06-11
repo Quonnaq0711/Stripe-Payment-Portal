1fea7ad5260756694d46e624817f52e11310d681
Quonnaq0711 <shaquonna.lynna@gmail.com>
2026-06-11 09:39:41 -0400

    debug

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,7 +8,7 @@
  * @module
  */
 
-import type * as http from "../http.js";
+import type * as https from "../http.js";
 import type * as users from "../users.js";
 
 import type {
@@ -18,7 +18,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
-  http: typeof http;
+  https: typeof https;
   users: typeof users;
 }>;
 
